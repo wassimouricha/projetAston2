687966c5df8fda795f9047545061d45ea2300b00
Bour1cha <wassim.bouricha@digi-service.fr>
2023-02-03 14:53:43 +0100

    setup commit

---
+++ popcorn/app/screens/LoginScreen.tsx
@@ -0,0 +1,183 @@
+import { observer } from "mobx-react-lite"
+import React, { FC, useEffect, useMemo, useRef, useState } from "react"
+import { TextInput, TextStyle, ViewStyle , Image, ImageStyle} from "react-native"
+import { Button, Icon, Screen, Text, TextField, TextFieldAccessoryProps } from "../components"
+import { useStores } from "../models"
+import { AppStackScreenProps } from "../navigators"
+import { colors, spacing } from "../theme"
+import { isRTL } from "../i18n"
+
+interface LoginScreenProps extends AppStackScreenProps<"Login"> {}
+const logo = require("../../assets/images/logopop.png")
+const popcornlogo = require("../../assets/images/popcornlogo.png")
+
+export const LoginScreen: FC<LoginScreenProps> = observer(function LoginScreen(_props) {
+  const authPasswordInput = useRef<TextInput>()
+  const [isAuthPasswordHidden, setIsAuthPasswordHidden] = useState(true)
+  const [isSubmitted, setIsSubmitted] = useState(false)
+  const [attemptsCount, setAttemptsCount] = useState(0)
+  const {
+    authenticationStore: {
+      authEmail,
+      authPassword,
+      setAuthEmail,
+      setAuthPassword,
+      setAuthToken,
+      validationErrors,
+    },
+  } = useStores()
+
+  useEffect(() => {
+    // Here is where you could fetch credentials from keychain or storage
+    // ici je pré-remplis les inputs 
+    setAuthEmail("wassim.bouricha@popcorn.fr")
+    setAuthPassword("wassim")
+  }, [])
+
+  const errors: typeof validationErrors = isSubmitted ? validationErrors : ({} as any)
+
+  function login() {
+    setIsSubmitted(true)
+    setAttemptsCount(attemptsCount + 1)
+
+    if (Object.values(validationErrors).some((v) => !!v)) return
+
+    // fait une requete au serveur afin d'avoir le token d'authentification
+    // si ok , reset les champs et donne le token
+    setIsSubmitted(false)
+    setAuthPassword("")
+    setAuthEmail("")
+
+    // pour l'instant c'est un fake token.
+    setAuthToken(String(Date.now()))
+  }
+
+  const PasswordRightAccessory = useMemo(
+    () =>
+      function PasswordRightAccessory(props: TextFieldAccessoryProps) {
+        return (
+          <Icon
+            icon={isAuthPasswordHidden ? "view" : "hidden"}
+            color={colors.palette.neutral800}
+            containerStyle={props.style}
+            onPress={() => setIsAuthPasswordHidden(!isAuthPasswordHidden)}
+          />
+        )
+      },
+    [isAuthPasswordHidden],
+  )
+
+  useEffect(() => {
+    return () => {
+      setAuthPassword("")
+      setAuthEmail("")
+    }
+  }, [])
+
+  return (
+    <Screen
+      preset="auto"
+      contentContainerStyle={$screenContentContainer}
+      safeAreaEdges={["top", "bottom"]}
+    >
+      {/* <Text testID="login-heading" tx="loginScreen.signIn" preset="heading" style={$signIn} /> */}
+      <Image style={$signupLogo} source={logo} resizeMode="contain" />
+      <Text text="Bienvenue sur "   preset="subheading" style={$enterDetails} />
+      <Image style={$popcornLogo} source={popcornlogo} resizeMode="contain" />
+      <Text text="Votre référence cinéma et séries"   preset="subheading" style={$enterDetails} />
+      {attemptsCount > 2 && <Text tx="loginScreen.hint" size="sm" weight="light" style={$hint} />}
+
+      <TextField
+        value={authEmail}
+        onChangeText={setAuthEmail}
+        containerStyle={$textField}
+        autoCapitalize="none"
+        autoComplete="email"
+        autoCorrect={false}
+        keyboardType="email-address"
+        labelTx="loginScreen.emailFieldLabel"
+        placeholderTx="loginScreen.emailFieldPlaceholder"
+        helper={errors?.authEmail}
+        status={errors?.authEmail ? "error" : undefined}
+        onSubmitEditing={() => authPasswordInput.current?.focus()}
+      />
+
+      <TextField
+        ref={authPasswordInput}
+        value={authPassword}
+        onChangeText={setAuthPassword}
+        containerStyle={$textField}
+        autoCapitalize="none"
+        autoComplete="password"
+        autoCorrect={false}
+        secureTextEntry={isAuthPasswordHidden}
+        label="Mot de passe"
+        placeholderTx="loginScreen.passwordFieldPlaceholder"
+        helper={errors?.authPassword}
+        status={errors?.authPassword ? "error" : undefined}
+        onSubmitEditing={login}
+        RightAccessory={PasswordRightAccessory}
+      />
+
+      <Button
+        testID="login-button"
+        text="Se connecter"
+        style={$tapButton}
+        preset="reversed"
+        onPress={login}
+      />
+      <Button
+        testID="login-button"
+        text="S'inscire"
+        style={$tapButton}
+        preset="reversed"
+        onPress={login}
+      />
+
+    <Text text="Continuer en tant que visiteur"   preset="subheading" style={$hint} />
+    </Screen>
+  )
+})
+
+const $screenContentContainer: ViewStyle = {
+  paddingVertical: spacing.huge,
+  paddingHorizontal: spacing.large,
+}
+
+const $signIn: TextStyle = {
+  marginBottom: spacing.small,
+}
+
+const $enterDetails: TextStyle = {
+  marginBottom: spacing.large,
+}
+
+const $hint: TextStyle = {
+  color: colors.tint,
+  marginBottom: spacing.medium,
+}
+
+const $textField: ViewStyle = {
+  marginBottom: spacing.large,
+}
+
+const $tapButton: ViewStyle = {
+  marginTop: spacing.extraSmall,
+}
+
+const $signupLogo: ImageStyle = {
+  display:"flex",
+  justifyContent:"center",
+  height: 269,
+  width: 350,
+  alignItems: "center",
+  transform: [{ scaleX: isRTL ? -1 : 1 }],
+}
+const $popcornLogo: ImageStyle = {
+  display:"flex",
+  justifyContent:"center",
+  width: 350,
+  alignItems: "center",
+}
+
+
