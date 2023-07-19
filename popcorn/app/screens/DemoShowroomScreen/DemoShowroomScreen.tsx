72074237010617fcfe53f0d5f3cddd8db201b539
Bour1cha <wassim.bouricha@digi-service.fr>
2023-07-19 15:16:12 +0200

    home page v1

--- popcorn/app/screens/DemoShowroomScreen/DemoShowroomScreen.tsx
+++ popcorn/app/screens/DemoShowroomScreen/DemoShowroomScreen.tsx
@@ -1,35 +1,114 @@
 import { Link, RouteProp, useRoute } from "@react-navigation/native"
 import React, { FC, ReactElement, useEffect, useRef, useState } from "react"
 import {
-  Dimensions,
-  FlatList,
   Image,
   ImageStyle,
-  Platform,
   SectionList,
   TextStyle,
   View,
   ViewStyle,
   ScrollView,
   ImageBackground, 
-  StyleSheet,
+  TextInput ,
 } from "react-native"
-import { DrawerLayout, DrawerState } from "react-native-gesture-handler"
-import { useSharedValue, withTiming } from "react-native-reanimated"
-import { Card, ListItem, Screen, Text } from "../../components"
-import { isRTL } from "../../i18n"
-import { DemoTabParamList, DemoTabScreenProps } from "../../navigators/DemoNavigator"
+import {   Screen, Text } from "../../components"
+import { DemoTabScreenProps } from "../../navigators/DemoNavigator"
 import { colors, spacing } from "../../theme"
-import { useSafeAreaInsetsStyle } from "../../utils/useSafeAreaInsetsStyle"
 import * as Demos from "./demos"
-import { DrawerIconButton } from "./DrawerIconButton"
+
 
 const logo = require("../../../assets/images/logo.png")
-const spidey = require("../../../assets/images/spiderman.jpg")
-const titreFilm =  "Le titre du film Complet"
-const genreFilm = "Genre"
-const anneeFilm = "Année"
-const dureeFilm = "Durée"
+const thierry = require("../../../assets/images/thierry.jpg")
+const films = [
+  {
+    affiche: require("../../../assets/images/spiderman.jpg"),
+    genre: "Action",
+    annee: "2023",
+    duree: "2h30min",
+    titre: "Spider-Man: Across the spider-verse",
+  },
+  {
+    affiche: require("../../../assets/images/gardian.jpg"),
+    genre: "Action",
+    annee: "2023",
+    duree: "2h30min",
+    titre: "Gardiens de la galaxie 3",
+  },
+  {
+    affiche: require("../../../assets/images/wick.jpg"),
+    genre: "Action",
+    annee: "2023",
+    duree: "2h49min",
+    titre: "John Wick - Chapitre 4",
+  },
+  {
+    affiche: require("../../../assets/images/fraise.jpg"),
+    genre: "Drame",
+    annee: "2023",
+    duree: "1H39min",
+    titre: "Omar la fraise",
+  },
+]
+const series = [
+  {
+    affiche: require("../../../assets/images/lastof.jpeg"),
+    genre: "Drame",
+    annee: "2023",
+    duree: "1 saison",
+    titre: "The Last of us",
+  },
+  {
+    affiche: require("../../../assets/images/boys.jpg"),
+    genre: "Action",
+    annee: "2019",
+    duree: "3 saisons",
+    titre: "The Boys",
+  },
+  {
+    affiche: require("../../../assets/images/mando.jpg"),
+    genre: "Space western",
+    annee: "2019",
+    duree: "3 saisons",
+    titre: "The Mandalorian",
+  },
+  {
+    affiche: require("../../../assets/images/bear.jpg"),
+    genre: "Comédie dramatique",
+    annee: "2022",
+    duree: "1 saison",
+    titre: "The bear",
+  },
+]
+const FilmMayLike = [
+  {
+    affiche: require("../../../assets/images/matrix.jpg"),
+    genre: "SF",
+    duree: "2h 15min",
+    titre: "Matrix",
+  },
+  {
+    affiche: require("../../../assets/images/nope.jpg"),
+    genre: "Horreur",
+    annee: "2022",
+    duree: "2h 10min",
+    titre: "Nope",
+  },
+  {
+    affiche: require("../../../assets/images/prey.jpeg"),
+    genre: "Action/SF",
+    annee: "2019",
+    duree: "1h 40min",
+    titre: "Prey",
+  },
+  {
+    affiche: require("../../../assets/images/kimi.jpg"),
+    genre: "Comédie dramatique",
+    annee: "2022",
+    duree: "1h 29min",
+    titre: "KIMI",
+  },
+]
+
 
 export interface Demo {
   name: string
@@ -43,172 +122,63 @@ interface DemoListItem {
   handleScroll?: (sectionIndex: number, itemIndex?: number) => void
 }
 
-const slugify = (str) =>
-  str
-    .toLowerCase()
-    .trim()
-    .replace(/[^\w\s-]/g, "")
-    .replace(/[\s_-]+/g, "-")
-    .replace(/^-+|-+$/g, "")
 
-const WebListItem: FC<DemoListItem> = ({ item, sectionIndex }) => {
-  const sectionSlug = item.name.toLowerCase()
-
-  return (
-    <View>
-      <Link to={`/showroom/${sectionSlug}`} style={$menuContainer}>
-        <Text preset="bold">{item.name}</Text>
-      </Link>
-      {item.useCases.map((u) => {
-        const itemSlug = slugify(u)
-
-        return (
-          <Link key={`section${sectionIndex}-${u}`} to={`/showroom/${sectionSlug}/${itemSlug}`}>
-            <Text>{u}</Text>
-          </Link>
-        )
-      })}
-    </View>
-  )
-}
-
-const NativeListItem: FC<DemoListItem> = ({ item, sectionIndex, handleScroll }) => {
-  return (
-    <View>
-      <Text onPress={() => handleScroll(sectionIndex)} preset="bold" style={$menuContainer}>
-        {item.name}
-      </Text>
-      {item.useCases.map((u, index) => (
-        <ListItem
-          key={`section${sectionIndex}-${u}`}
-          onPress={() => handleScroll(sectionIndex, index + 1)}
-          text={u}
-          rightIcon={isRTL ? "caretLeft" : "caretRight"}
-        />
-      ))}
-    </View>
-  )
-}
-
-const ShowroomListItem = Platform.select({ web: WebListItem, default: NativeListItem })
 
 export const DemoShowroomScreen: FC<DemoTabScreenProps<"DemoShowroom">> =
   function DemoShowroomScreen(_props) {
-    const [open, setOpen] = useState(false)
-    const timeout = useRef<ReturnType<typeof setTimeout>>()
-    const drawerRef = useRef<DrawerLayout>()
+
     const listRef = useRef<SectionList>()
-    const menuRef = useRef<FlatList>()
-    const progress = useSharedValue(0)
-    const route = useRoute<RouteProp<DemoTabParamList, "DemoShowroom">>()
-    const params = route.params
+
     const loggedName = "Thierry"
 
-    // handle Web links
-    React.useEffect(() => {
-      if (route.params) {
-        const demoValues = Object.values(Demos)
-        const findSectionIndex = demoValues.findIndex(
-          (x) => x.name.toLowerCase() === params.queryIndex,
-        )
-        let findItemIndex = 0
-        if (params.itemIndex) {
-          try {
-            findItemIndex =
-              demoValues[findSectionIndex].data.findIndex(
-                (u) => slugify(u.props.name) === params.itemIndex,
-              ) + 1
-          } catch (err) {
-            console.error(err)
-          }
-        }
-        handleScroll(findSectionIndex, findItemIndex)
-      }
-    }, [route])
-
-    const toggleDrawer = () => {
-      if (!open) {
-        setOpen(true)
-        drawerRef.current?.openDrawer({ speed: 2 })
-      } else {
-        setOpen(false)
-        drawerRef.current?.closeDrawer({ speed: 2 })
-      }
-    }
-
-    const handleScroll = (sectionIndex: number, itemIndex = 0) => {
-      listRef.current.scrollToLocation({
-        animated: true,
-        itemIndex,
-        sectionIndex,
-      })
-      toggleDrawer()
-    }
-
-    const scrollToIndexFailed = (info: {
-      index: number
-      highestMeasuredFrameIndex: number
-      averageItemLength: number
-    }) => {
-      listRef.current?.getScrollResponder()?.scrollToEnd()
-      timeout.current = setTimeout(
-        () =>
-          listRef.current?.scrollToLocation({
-            animated: true,
-            itemIndex: info.index,
-            sectionIndex: 0,
-          }),
-        50,
-      )
-    }
-
-    useEffect(() => {
-      return () => timeout.current && clearTimeout(timeout.current)
-    }, [])
-
-    const $drawerInsets = useSafeAreaInsetsStyle(["top"])
+    // mon composant de carte film contenu dans le scrollview horizontal
+    const FilmItem = ({ affiche, genre, annee, duree, titre }) => (
+      <View style={{ flex: 1, borderRadius: 30, overflow: 'hidden', marginTop: 20, marginHorizontal: 10 }}>
+        <ImageBackground source={affiche} style={{ flex: 1 }}>
+          <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <View style={{ paddingTop: 40 }}>
+              <View style={{ height: 200 }}>
+                <View style={{ flex: 2, width: 330, flexDirection: 'column-reverse' }}>
+                  <View style={{ flexDirection: 'row', justifyContent: 'space-between' , marginHorizontal:15}}>
+                    <Text preset="subheading" style={{ color: colors.palette.neutral100  , fontSize:12}}>{genre}</Text>
+                    <Text preset="subheading" style={{ color: colors.palette.neutral100 , fontSize:12 }}>{annee}</Text>
+                    <Text preset="subheading" style={{ color: colors.palette.neutral100, fontSize:12 }}>{duree}</Text>
+                  </View>
+                  <Text preset="subheading" style={{ color: colors.palette.neutral100, alignItems: 'center' ,marginHorizontal:15}}>{titre}</Text>
+                </View>
+              </View>
+            </View>
+          </View>
+        </ImageBackground>
+      </View>
+    );
+    
+    // mon deuxieme composant de carte film contenu dans le scrollview horizontal mais différent
+    const FilmMayLikeItem = ({ affiche, genre, duree, titre }) => (
+      <View style={{ flex: 1, borderRadius: 30, overflow: 'hidden', marginTop: 20, marginHorizontal: 10  , width:150}}>
+        <ImageBackground source={affiche} style={{ flex: 1 }}>
+          <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <View style={{ paddingTop: 40 }}>
+              <View style={{ height: 200 }}>
+                <View style={{ flex: 2, width: 150, flexDirection: 'column-reverse' ,justifyContent: 'space-between' }}>
+                  <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginHorizontal:15}}>
+                    <Text preset="subheading" style={{ color: colors.palette.neutral100  , fontSize:8}}>{genre}</Text>
+                    <Text preset="subheading" style={{ color: colors.palette.neutral100, fontSize:8 }}>{duree}</Text>
+                  </View>
+                  <Text preset="subheading" style={{ color: colors.palette.neutral100, alignItems: 'center' ,marginHorizontal:15 ,  fontSize:8 , width:120}}>{titre}</Text>
+                </View>
+              </View>
+            </View>
+          </View>
+        </ImageBackground>
+      </View>
+    );
+    
 
     return (
-      <DrawerLayout
-        ref={drawerRef}
-        drawerWidth={Platform.select({ default: 326, web: Dimensions.get("window").width * 0.3 })}
-        drawerType={"slide"}
-        drawerPosition={isRTL ? "right" : "left"}
-        overlayColor={open ? colors.palette.overlay20 : "transparent"}
-        onDrawerSlide={(drawerProgress) => {
-          progress.value = open ? 1 - drawerProgress : drawerProgress
-        }}
-        onDrawerStateChanged={(newState: DrawerState, drawerWillShow: boolean) => {
-          if (newState === "Settling") {
-            progress.value = withTiming(drawerWillShow ? 1 : 0, {
-              duration: 250,
-            })
-            setOpen(drawerWillShow)
-          }
-        }}
-        renderNavigationView={() => (
-          <View style={[$drawer, $drawerInsets]}>
-            <View style={$logoContainer}>
-              <Image source={logo} style={$logoImage} />
-            </View>
 
-            <FlatList<{ name: string; useCases: string[] }>
-              ref={menuRef}
-              contentContainerStyle={$flatListContentContainer}
-              data={Object.values(Demos).map((d) => ({
-                name: d.name,
-                useCases: d.data.map((u) => u.props.name),
-              }))}
-              keyExtractor={(item) => item.name}
-              renderItem={({ item, index: sectionIndex }) => (
-                <ShowroomListItem {...{ item, sectionIndex, handleScroll }} />
-              )}
-            />
-          </View>
-        )}
-      >
         <Screen preset="fixed" safeAreaEdges={["top"]} contentContainerStyle={$screenContainer}>
-          <DrawerIconButton onPress={toggleDrawer} {...{ open, progress }} />
+
 
           <SectionList
             ref={listRef}
@@ -219,74 +189,68 @@ export const DemoShowroomScreen: FC<DemoTabScreenProps<"DemoShowroom">> =
             renderSectionFooter={() => <View style={$demoUseCasesSpacer} />}
             ListHeaderComponent={
 
-              <View style={$heading}>
-                   <Text preset="subheading" style={$heading}>
+              <View style={{marginTop:20}}>
+                <View style={{flexDirection:'row' , justifyContent:'center' , alignItems:'center'  }}>
+                  <Image source={thierry} style={{height: 70, width: 70, marginRight: 10 , borderRadius:40 , marginLeft :60}} />
+                  <View>
+                  <Text preset="subheading" style={ {width: 300 }}>
               Salut{" "}
               <Text preset="subheading" style={$loggedName}>
-                {loggedName}
+                {loggedName} 
               </Text>
-              {" !  C'est quoi ton film aujourd'hui ?"}
+              <Text> !</Text>
             </Text>
-            {/* <Card
-        heading=""
-        content={`${titreFilm} `}
-        footer={`${genreFilm}                 ${anneeFilm}                  ${dureeFilm}`}
-        contentStyle={{color: colors.palette.red , alignItems: 'center', justifyContent: 'center'}}
-        footerStyle={{color: colors.palette.red , alignItems: 'center', justifyContent: 'space-between'}}
-        // backgroundImage={spidey}
-       
-      > 
-      </Card> */}
+              <Text style={{width:250 , fontSize:12}}> C'est quoi ton film aujourd'hui ?</Text>
+                  </View>
+                </View>
+                <View style={{flexDirection:'row' , justifyContent:'center' , alignItems:'center' ,backgroundColor: colors.palette.gray, width:350 , borderRadius:40 , marginTop:10 , marginBottom:10}}>
+                   {/* Input de recherche */}
+                   <Image source={require("../../../assets/images/search.png")} style={{ height: 30, width: 30, marginRight:20}} />
+                      <TextInput
+                      placeholder="Rechercher votre film/série..."
+                      style={{ backgroundColor: colors.palette.gray, margin: 10, paddingHorizontal: 10, borderRadius: 8, height: 40 }}
+                      onChangeText={(text) => {
+
+                      }}
+                    />
+                
+                </View>
+
 
                     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
                       <Text preset="subheading" >Sortie de la semaine</Text>
                     </View>
-
-      <View style={{ flex: 1, borderRadius: 30, overflow: 'hidden' ,  marginTop: 20}}>
-      <ImageBackground source={spidey} style={{ flex: 1, }}>
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-          <ScrollView scrollEventThrottle={16}>
-            <View style={{  paddingTop: 40 }}>
-              <View style={{ height: 200 }}>
-                <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-                  <View style={{flex:2,   width: 330 , flexDirection: 'column-reverse' }}>
-                  <View style={{   flexDirection:'row' , justifyContent: 'space-between'}}>
-                  <Text preset="subheading" style={{color: colors.palette.neutral100 ,}}>Genre</Text>
-                  <Text preset="subheading" style={{color: colors.palette.neutral100 ,}}>Année</Text>
-                  <Text preset="subheading" style={{color: colors.palette.neutral100 ,}}>Durée</Text>
-                  </View>
-                  <Text preset="subheading" style={{color: colors.palette.neutral100 , alignItems:'center'}}>Titre du film complet</Text>
-                  </View>
-                </ScrollView>
-              </View>
-            </View>
-          </ScrollView>
-        </View>
-      </ImageBackground>
-    </View>
-
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+                      {/* Composant de scrolling horizontal contenant mes cartes des sorties de la semaine */}
+                    <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+                      {films.map((film, index) => (
+                        <FilmItem key={index} {...film} />
+                      ))}
+                    </ScrollView>
+        
+                <View style={{flex: 1, alignItems: 'center', justifyContent: 'center' , marginTop:20}}>
                       <Text preset="subheading" >Série à la une </Text>
                     </View>
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+                            {/* Composant de scrolling horizontal contenant mes cartes des series a la une */}
+                            <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+                      {series.map((film, index) => (
+                        <FilmItem key={index} {...film} />
+                      ))}
+                    </ScrollView>
+        
+              <View style={{flex: 1, alignItems: 'center', justifyContent: 'center' , marginTop:20}}>
                       <Text preset="subheading" >Vous aimerez peut-être </Text>
                     </View>
+
+                      {/* Composant de scrolling horizontal contenant mes cartes des sorties de la semaine */}
+                      <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+                      {FilmMayLike.map((film, index) => (
+                        <FilmMayLikeItem key={index} {...film} />
+                      ))}
+                    </ScrollView>
               </View>
             }
-            onScrollToIndexFailed={scrollToIndexFailed}
-            renderSectionHeader={({ section }) => {
-              return (
-                <View>
-                  <Text preset="heading" style={$demoItemName}>
-                    {section.name}
-                  </Text>
-                  <Text style={$demoItemDescription}>{section.description}</Text>
-                </View>
-              )
-            }}
           />
         </Screen>
-      </DrawerLayout>
     )
   }
 
