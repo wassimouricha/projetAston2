2dcbd0458e06f4b4a063a3c5e16d273a1dccdc84
Bour1cha <wassim.bouricha@digi-service.fr>
2023-07-20 16:15:37 +0200

    clean code

---
+++ popcorn/app/data/filmData.tsx
@@ -0,0 +1,92 @@
+import { ImageSourcePropType } from "react-native";
+
+export const films = [
+    {
+      affiche: require("../../assets/images/spiderman.jpg") as ImageSourcePropType ,
+      genre: "Action",
+      annee: "2023",
+      duree: "2h30min",
+      titre: "Spider-Man: Across the spider-verse",
+    },
+    {
+      affiche: require("../../assets/images/gardian.jpg") as ImageSourcePropType,
+      genre: "Action",
+      annee: "2023",
+      duree: "2h30min",
+      titre: "Gardiens de la galaxie 3",
+    },
+    {
+      affiche: require("../../assets/images/wick.jpg") as ImageSourcePropType,
+      genre: "Action",
+      annee: "2023",
+      duree: "2h49min",
+      titre: "John Wick - Chapitre 4",
+    },
+    {
+      affiche: require("../../assets/images/fraise.jpg") as ImageSourcePropType,
+      genre: "Drame",
+      annee: "2023",
+      duree: "1H39min",
+      titre: "Omar la fraise",
+    },
+  ]
+  export const series = [
+    {
+      affiche: require("../../assets/images/lastof.jpeg") as ImageSourcePropType,
+      genre: "Drame",
+      annee: "2023",
+      duree: "1 saison",
+      titre: "The Last of us",
+    },
+    {
+      affiche: require("../../assets/images/boys.jpg") as ImageSourcePropType,
+      genre: "Action",
+      annee: "2019",
+      duree: "3 saisons",
+      titre: "The Boys",
+    },
+    {
+      affiche: require("../../assets/images/mando.jpg") as ImageSourcePropType,
+      genre: "Space western",
+      annee: "2019",
+      duree: "3 saisons",
+      titre: "The Mandalorian",
+    },
+    {
+      affiche: require("../../assets/images/bear.jpg") as ImageSourcePropType,
+      genre: "Comédie dramatique",
+      annee: "2022",
+      duree: "1 saison",
+      titre: "The bear",
+    },
+  ]
+  export const FilmMayLike = [
+    {
+      affiche: require("../../assets/images/matrix.jpg") as ImageSourcePropType,
+      genre: "SF",
+      duree: "2h 15min",
+      titre: "Matrix",
+    },
+    {
+      affiche: require("../../assets/images/nope.jpg") as ImageSourcePropType,
+      genre: "Horreur",
+      annee: "2022",
+      duree: "2h 10min",
+      titre: "Nope",
+    },
+    {
+      affiche: require("../../assets/images/prey.jpeg") as ImageSourcePropType,
+      genre: "Action/SF",
+      annee: "2019",
+      duree: "1h 40min",
+      titre: "Prey",
+    },
+    {
+      affiche: require("../../assets/images/kimi.jpg") as ImageSourcePropType,
+      genre: "Comédie dramatique",
+      annee: "2022",
+      duree: "1h 29min",
+      titre: "KIMI",
+    },
+  ]
+  
--- popcorn/app/screens/HomePage.tsx
+++ popcorn/app/screens/HomePage.tsx
@@ -5,101 +5,11 @@ import {  Button, Screen, Text, } from "../components"
 import { colors, spacing } from "../theme"
 import { isRTL } from "../i18n"
 import { AppStackScreenProps } from "../navigators/AppNavigator"
-import { StackNavigationProp } from "@react-navigation/stack";
+import { films, series, FilmMayLike } from "../data/filmData"
+
 
 interface HomePageProps  extends AppStackScreenProps<"Home"> {titre?: string; annee?: string;}
-const logo = require("../../assets/images/logopop.png")
-const popcornlogo = require("../../assets/images/popcornlogo.png")
 const thierry = require("../../assets/images/thierry.jpg")
-const films = [
-  {
-    affiche: require("../../assets/images/spiderman.jpg") as ImageSourcePropType ,
-    genre: "Action",
-    annee: "2023",
-    duree: "2h30min",
-    titre: "Spider-Man: Across the spider-verse",
-  },
-  {
-    affiche: require("../../assets/images/gardian.jpg") as ImageSourcePropType,
-    genre: "Action",
-    annee: "2023",
-    duree: "2h30min",
-    titre: "Gardiens de la galaxie 3",
-  },
-  {
-    affiche: require("../../assets/images/wick.jpg") as ImageSourcePropType,
-    genre: "Action",
-    annee: "2023",
-    duree: "2h49min",
-    titre: "John Wick - Chapitre 4",
-  },
-  {
-    affiche: require("../../assets/images/fraise.jpg") as ImageSourcePropType,
-    genre: "Drame",
-    annee: "2023",
-    duree: "1H39min",
-    titre: "Omar la fraise",
-  },
-]
-const series = [
-  {
-    affiche: require("../../assets/images/lastof.jpeg") as ImageSourcePropType,
-    genre: "Drame",
-    annee: "2023",
-    duree: "1 saison",
-    titre: "The Last of us",
-  },
-  {
-    affiche: require("../../assets/images/boys.jpg") as ImageSourcePropType,
-    genre: "Action",
-    annee: "2019",
-    duree: "3 saisons",
-    titre: "The Boys",
-  },
-  {
-    affiche: require("../../assets/images/mando.jpg") as ImageSourcePropType,
-    genre: "Space western",
-    annee: "2019",
-    duree: "3 saisons",
-    titre: "The Mandalorian",
-  },
-  {
-    affiche: require("../../assets/images/bear.jpg") as ImageSourcePropType,
-    genre: "Comédie dramatique",
-    annee: "2022",
-    duree: "1 saison",
-    titre: "The bear",
-  },
-]
-const FilmMayLike = [
-  {
-    affiche: require("../../assets/images/matrix.jpg") as ImageSourcePropType,
-    genre: "SF",
-    duree: "2h 15min",
-    titre: "Matrix",
-  },
-  {
-    affiche: require("../../assets/images/nope.jpg") as ImageSourcePropType,
-    genre: "Horreur",
-    annee: "2022",
-    duree: "2h 10min",
-    titre: "Nope",
-  },
-  {
-    affiche: require("../../assets/images/prey.jpeg") as ImageSourcePropType,
-    genre: "Action/SF",
-    annee: "2019",
-    duree: "1h 40min",
-    titre: "Prey",
-  },
-  {
-    affiche: require("../../assets/images/kimi.jpg") as ImageSourcePropType,
-    genre: "Comédie dramatique",
-    annee: "2022",
-    duree: "1h 29min",
-    titre: "KIMI",
-  },
-]
 
 
 export const HomePage: FC<HomePageProps> = observer(function HomePage(_props) {
