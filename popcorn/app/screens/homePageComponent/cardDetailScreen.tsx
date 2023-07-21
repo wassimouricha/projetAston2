0793c95b305efa1971a3c2de271b83c6f8ca42a1
Bour1cha <wassim.bouricha@digi-service.fr>
2023-07-21 15:27:37 +0200

    card detail start

--- popcorn/app/screens/homePageComponent/cardDetailScreen.tsx
+++ popcorn/app/screens/homePageComponent/cardDetailScreen.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
-import { View, Text, ViewStyle, Image, ImageSourcePropType } from 'react-native';
-
+import { View, Text, ViewStyle, Image, ImageSourcePropType, ImageBackground } from 'react-native';
 
 const CardDetailScreen = ({ route }) => {
-  const { titre, annee , affiche} = route.params;
+  const { titre, annee, affiche } = route.params;
 
   return (
-    
     <View style={$screenContentContainer}>
-      <Image source={affiche} style={{ width: 100, height: 100 }} />
-      <Text>Film Detail Screen</Text>
-      <Text>Title: {titre}</Text>
-      <Text>Year: {annee}</Text>
-      {/* Add more content for the film detail */}
+      <ImageBackground
+        source={affiche}
+        style={{ height: 450, alignItems: 'center' }}
+      >
+        <View style={$bottomOverlay} />
+
+      </ImageBackground>
+      <View style={ $smallContentContainer}>
+        <Text>Film</Text>
+        <View style={$titleContainer}>
+        <Text> {titre}</Text>
+        <Text>Logo</Text>
+        </View>
+        <View style={$titleContainer}>
+        <Text>Genre : {titre}</Text>
+        <Text>Durée : </Text>
+        </View>
+
+      </View>
     </View>
   );
 };
@@ -20,7 +32,28 @@ const CardDetailScreen = ({ route }) => {
 export default CardDetailScreen;
 
 const $screenContentContainer: ViewStyle = {
-  display:"flex",
-  alignContent:"center",
+  display: "flex",
+  alignContent: "center",
+}
 
+const $smallContentContainer: ViewStyle = {
+  display: "flex",
+  paddingHorizontal: 20,
+}
+
+const $titleContainer: ViewStyle = {
+  display: "flex",
+  flexDirection: 'row',
+  justifyContent:'space-between'
+}
+
+
+
+const $bottomOverlay: ViewStyle = {
+  position: 'absolute',
+  bottom: 0,
+  left: 0,
+  right: 0,
+  height: 20, // Ajustez la hauteur de la zone de transparence ici
+  backgroundColor: 'linear-gradient(rgba(255, 255, 255, 0.5), rgba(0, 0, 0, ))', // Dégradé linéaire pour l'effet de transparence inversé
 }
