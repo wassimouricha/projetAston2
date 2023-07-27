d3d34048a2009a5626bb575241209bbdad2adf2d
Bour1cha <wassim.bouricha@digi-service.fr>
2023-07-27 17:48:23 +0200

    logged profile Data

---
+++ popcorn/app/data/loggedProfileData.tsx
@@ -0,0 +1,20 @@
+import { ImageSourcePropType } from "react-native";
+
+export const LoggedProfile = [
+    {
+      photo: require("../../assets/images/thierry.jpg") as ImageSourcePropType ,
+      nom: "Henry",
+      prenom: "Thierry",
+      description: "Thierry Henry, né le 17 août 1977 aux Ulis , est un footballeur international français qui a évolué au poste d'attaquant entre 1994 et 2014. Il est depuis 2016 entraîneur adjoint de l'équipe de Belgique.",
+      mail: "titi@gmail.com",
+      age: "50 ans",
+      adresse: "Monaco,France",
+      statut: "Légende",
+      amis: 35,
+      likes: 55,
+      dislikes: 27,
+      fav: 123,
+    },
+
+  ]
+ 
